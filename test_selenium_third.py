22912e48e6ea0da531fee72c7ba9a8f66da4bc60
petro <petro25@gmail.com>
2017-06-16 00:33:46 +0300

    Done: Auto open menu and submenu
    Not Done:
    Check H1 header on each page

--- test_selenium_third.py
+++ test_selenium_third.py
@@ -6,10 +6,10 @@ from selenium.webdriver.common.by import By
 
 def test_my_first():
     #driver = webdriver.Safari()
-    #driver = webdriver.Firefox()
+    #driver = webdriver.Firefox('/Users/petro/Downloads/geckodriver')
     driver = webdriver.Chrome('/Users/petro/Downloads/chromedriver')
 
-    #wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 5)
 
     driver.get('http://127.0.0.1/litecart/admin/')
     login_filed = driver.find_element_by_name('username')
@@ -19,24 +19,26 @@ def test_my_first():
     login_button = driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button')
     login_button.click()
 
-    appearance_menu = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#app- > a > span.name')))
-    appearance_menu = driver.find_element_by_css_selector('#app- > a > span.name')
-    appearance_menu.click()
-
-
-    logotype = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#doc-logotype > a > span')))
-    logotype = driver.find_element_by_css_selector('#doc-logotype > a > span')
-    logotype.click()
-
-    #driver.get('http://127.0.0.1/litecart/admin/')
-    #HERE SLENIUM CLICKS ON FIRST MENU(I DO NOT KNOW WHY)
-    catalog = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#app- > a > span.name')))
-    catalog = driver.find_element_by_css_selector('#app- > a > span.name')
-    catalog.click()
-
-    prod_group = WebDriverWait(driver, 5).until(ec.element_to_be_clickable(By.CSS_SELECTOR, '#doc-product_groups > a > span'))
-    prod_group = driver.find_element_by_css_selector('#doc-product_groups > a > span')
-    prod_group.click()
+    counter_menu = 1
+    while True:
+        try:
+            el_menu = wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="box-apps-menu"]/li['+str(counter_menu)+']')))
+            menu = driver.find_element_by_xpath('//*[@id="box-apps-menu"]/li['+str(counter_menu)+']')
+            menu.click()
+            time.sleep(1)
+            #conuntet_submenu can start from 2 because first submenu loaded by default
+            counter_submenu = 1
+            while True:
+                try:
+                    submenu = driver.find_element_by_xpath('//*[@id="box-apps-menu"]/li['+str(counter_menu)+']/ul/li['+str(counter_submenu)+']')
+                    submenu.click()
+                    time.sleep(1)
+                    counter_submenu = counter_submenu + 1
+                except:
+                    break
+            counter_menu = counter_menu + 1
+        except:
+            break
 
     time.sleep(2)
     driver.quit()
